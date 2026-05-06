2f57eb622a56836989616ca350cdf0cd5dad4b13
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 15:27:37 +0400

    Assgin value for testing

--- src/FactorialProgram.java
+++ src/FactorialProgram.java
@@ -1,5 +1,10 @@
 public class FactorialProgram {
     public static void main(String[] args) {
-        Factorial factorial = new Factorial();
+        Factorial fact = new Factorial();
+
+        int result1 = fact.factorial(1);
+        int result2 = fact.factorial(2);
+        int result3 = fact.factorial(3);
+
     }
 }
