3edbdffea9de38ea3d1f1cc902be55b874591c2d
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 22:10:35 +0400

    Display answer of Testing cases

--- src/Count8Demo.java
+++ src/Count8Demo.java
@@ -5,5 +5,9 @@ public class Count8Demo {
         int result1 = count8.count8(8);
         int result2 = count8.count8(818);
         int result3 = count8.count8(8818);
+
+        System.out.println("count8(8) → " + result1);
+        System.out.println("count8(818) → " + result2);
+        System.out.println("count8(8818) → " + result3);
     }
 }
