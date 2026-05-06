c6ed4063246617846c2d34fac1102f6ffb460cad
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:27:48 +0400

    Calling methods for checking

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -6,6 +6,10 @@ public class Array11Demo {
         int[] arr2 = {11, 11};
         int[] arr3 = {1, 2, 3, 4};
 
+        int result1 = array11.array11(arr1, 0);
+        int result2 = array11.array11(arr2, 0);
+        int result3 = array11.array11(arr3, 0);
+
 
     }
 }
