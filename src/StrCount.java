d3e2b7465a4ae2ff107395c07ed58e41dc7ed310
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 21:07:01 +0400

    Otherwise if not match with sub

--- src/StrCount.java
+++ src/StrCount.java
@@ -7,5 +7,7 @@ public class StrCount {
         if (str.substring(0,sub.length()).equals(sub)) {
             return 1 + strCount(str.substring(sub.length()),sub);
         }
+
+        return strCount(str.substring(1), sub);
     }
 }
