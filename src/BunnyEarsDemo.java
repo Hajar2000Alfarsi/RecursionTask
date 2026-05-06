03137c3f1fc91a16a03684bdd7409522bd8726f5
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 15:40:00 +0400

    Implement main method

--- src/BunnyEarsDemo.java
+++ src/BunnyEarsDemo.java
@@ -1,8 +1,12 @@
 public class BunnyEarsDemo {
+    public static void main(String[] args) {
+
     BunnyEars bunny = new BunnyEars();
 
     int result0 = bunny.bunnyEars(0);
     int result1 = bunny.bunnyEars(1);
     int result2 = bunny.bunnyEars(2);
 
+
+    }
 }
