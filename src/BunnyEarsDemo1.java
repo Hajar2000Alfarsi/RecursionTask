517fd24110be7b4ed1c9121d4b2948f9280dfb1a
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 21:21:36 +0400

    Testing Examples

--- src/BunnyEarsDemo1.java
+++ src/BunnyEarsDemo1.java
@@ -2,5 +2,9 @@ public class BunnyEarsDemo1 {
     public static void main(String[] args) {
         BunnyEars1 bunnyEars1 = new BunnyEars1();
 
+        int result1 = bunnyEars1.bunnyEars(0);
+        int result2 = bunnyEars1.bunnyEars(1);
+        int result3 = bunnyEars1.bunnyEars(2);
+
     }
 }
