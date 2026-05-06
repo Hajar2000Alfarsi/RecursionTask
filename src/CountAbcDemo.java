df2bc07743811c33dbf5a53416768e545f502295
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 20:13:39 +0400

    Show result of exaples

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -5,5 +5,9 @@ public class CountAbcDemo {
         int result1 = countAbc.countAbc("abc");
         int result2 = countAbc.countAbc("abcxxabc");
         int result3 = countAbc.countAbc("abaxxaba");
+
+        System.out.println("countAbc(\"abc\") = " + result1);
+        System.out.println("countAbc(\"abcxxabc\") = " + result2);
+        System.out.println("countAbc(\"abaxxaba\") = " + result3);
     }
 }
