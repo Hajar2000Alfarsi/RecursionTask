8d46dcab49622a9ec1af6b4648b4f47c8d971e57
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 20:12:58 +0400

    Exapmple for Test String

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -2,5 +2,8 @@ public class CountAbcDemo {
     public static void main(String[] args) {
         CountAbc countAbc = new CountAbc();
 
+        int result1 = countAbc.countAbc("abc");
+        int result2 = countAbc.countAbc("abcxxabc");
+        int result3 = countAbc.countAbc("abaxxaba");
     }
 }
