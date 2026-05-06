ba51a31908acef79553811bf1078f951fb9381fa
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:46:31 +0400

    Result for the testing

--- src/PairStarDemo.java
+++ src/PairStarDemo.java
@@ -5,5 +5,9 @@ public class PairStarDemo {
         String result1 = pairStar.pairStar("hello");
         String result2 = pairStar.pairStar("xxyy");
         String result3 = pairStar.pairStar("aaaa");
+
+        System.out.println("pairStar(\"hello\") = " + result1);
+        System.out.println("pairStar(\"xxyy\") = " + result2);
+        System.out.println("pairStar(\"aaaa\") = " + result3);
     }
 }
