17f7b75756e94b51e1127beb449b03b304d11a73
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 22:10:10 +0400

    Testing cases

--- src/Count8Demo.java
+++ src/Count8Demo.java
@@ -2,5 +2,8 @@ public class Count8Demo {
     public static void main(String[] args) {
         Count8 count8 = new Count8();
 
+        int result1 = count8.count8(8);
+        int result2 = count8.count8(818);
+        int result3 = count8.count8(8818);
     }
 }
