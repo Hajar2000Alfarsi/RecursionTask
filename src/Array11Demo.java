a60aec4bbb867398099770337e5d3e038f473c47
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:26:38 +0400

    Testing vlaues

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -2,5 +2,10 @@ public class Array11Demo {
     public static void main(String[] args) {
         Array11 array11 = new Array11();
 
+        int[] arr1 = {1, 2, 11};
+        int[] arr2 = {11, 11};
+        int[] arr3 = {1, 2, 3, 4};
+
+
     }
 }
