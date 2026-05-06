3ea4450f7945405866da1d68404c0a1355e04d5e
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 15:53:42 +0400

    Testing value

--- src/Count7Demo.java
+++ src/Count7Demo.java
@@ -2,5 +2,8 @@ public class Count7Demo {
     public static void main(String[] args) {
         Count7 count = new Count7();
 
+        int result1 = count.count7(717);
+        int result2 = count.count7(7);
+        int result3 = count.count7(123);
     }
 }
