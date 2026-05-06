13f0e4bd0742068b6275fbd4ca42fadf33bfe99e
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:18:37 +0400

    Display result of testing Strings

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -5,5 +5,9 @@ public class ChangePiDemo {
         String result1 = changePi.changePi("xpix");
         String result2 = changePi.changePi("pipi");
         String result3 = changePi.changePi("pip");
+
+        System.out.println("changePi(\"xpix\") = " + result1);
+        System.out.println("changePi(\"pipi\") = " + result2);
+        System.out.println("changePi(\"pip\") = " + result3);
     }
 }
