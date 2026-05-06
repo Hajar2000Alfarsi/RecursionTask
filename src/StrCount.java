89859e0956556a09a1577e9457fa34563317065a
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 21:05:19 +0400

    Check if begining of String match sub

--- src/StrCount.java
+++ src/StrCount.java
@@ -4,5 +4,8 @@ public class StrCount {
             return 0;
         }
 
+        if (str.substring(0,sub.length()).equals(sub)) {
+            return 1 + strCount(str.substring(sub.length()),sub);
+        }
     }
 }
