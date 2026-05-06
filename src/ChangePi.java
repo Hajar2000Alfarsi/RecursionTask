b8ab0dd484941693b6f8a323aa0a63a9cc33c733
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:12:39 +0400

    Check if String start with pi

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -3,5 +3,9 @@ public class ChangePi {
         if (str.length() == 0) {
             return "";
         }
+
+        if (str.length() >= 2 && str.substring(0,2).equals("pi")) {
+            return "3.14" + changePi(str.substring(2));
+        }
     }
 }
