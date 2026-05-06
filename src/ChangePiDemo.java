63ecb48406828462c1812bdd1a1718eb390c360f
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:17:48 +0400

    Testing Strings

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -2,5 +2,8 @@ public class ChangePiDemo {
     public static void main(String[] args) {
         ChangePi changePi = new ChangePi();
 
+        String result1 = changePi.changePi("xpix");
+        String result2 = changePi.changePi("pipi");
+        String result3 = changePi.changePi("pip");
     }
 }
