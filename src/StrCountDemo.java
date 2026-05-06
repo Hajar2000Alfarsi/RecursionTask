ca07dd42e0e4d3e65b5a419afa129b683996bace
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 21:11:32 +0400

    Display the result

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -6,5 +6,8 @@ public class StrCountDemo {
         int result2 = strCount.strCount("catcowcat", "cow");
         int result3 = strCount.strCount("catcowcat", "dog");
 
+        System.out.println("strCount(\"catcowcat\", \"cat\") → " + result1);
+        System.out.println("strCount(\"catcowcat\", \"cow\") → " + result2);
+        System.out.println("strCount(\"catcowcat\", \"dog\") → " + result3);
     }
 }
