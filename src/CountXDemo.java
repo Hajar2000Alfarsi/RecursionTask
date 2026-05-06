b951a6cad7b7961426502c757d3f52dc1bb6a7e0
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:04:41 +0400

    Testing String

--- src/CountXDemo.java
+++ src/CountXDemo.java
@@ -1,5 +1,11 @@
 public class CountXDemo {
     public static void main(String[] args) {
         CountX countX = new CountX();
+
+        int result1 = countX.countX("xxhixx");
+        int result2 = countX.countX("xhixhix");
+        int result3 = countX.countX("hi");
+
+
     }
 }
