a9f3f1539ea0410554ac9d1035e163a5ad739c48
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:45:49 +0400

    Testing Strings

--- src/PairStarDemo.java
+++ src/PairStarDemo.java
@@ -2,5 +2,8 @@ public class PairStarDemo {
     public static void main(String[] args) {
         PairStar pairStar = new PairStar();
 
+        String result1 = pairStar.pairStar("hello");
+        String result2 = pairStar.pairStar("xxyy");
+        String result3 = pairStar.pairStar("aaaa");
     }
 }
