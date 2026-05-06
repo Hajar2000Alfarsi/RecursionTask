fa7e1da46aa240e92ee94ea906fe56d08b819f18
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 21:10:47 +0400

    Examples for testing

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -1,5 +1,10 @@
 public class StrCountDemo {
     public static void main(String[] args) {
         StrCount strCount = new StrCount();
+
+        int result1 = strCount.strCount("catcowcat", "cat");
+        int result2 = strCount.strCount("catcowcat", "cow");
+        int result3 = strCount.strCount("catcowcat", "dog");
+
     }
 }
