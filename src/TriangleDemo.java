b929c6742ca6cf1d4daa2a969d95f762cf67c156
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 21:38:38 +0400

    Test example

--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -1,5 +1,9 @@
 public class TriangleDemo {
     public static void main(String[] args) {
         Triangle triangle = new Triangle();
+
+        int result1 = triangle.triangle(0);
+        int result2 = triangle.triangle(1);
+        int result3 = triangle.triangle(2);
     }
 }
