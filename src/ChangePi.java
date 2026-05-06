9e668ee031cb96d13c186ea109ac6e2f411049a2
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 16:14:44 +0400

    Otherwise return first char and continue recursion

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -7,5 +7,8 @@ public class ChangePi {
         if (str.length() >= 2 && str.substring(0,2).equals("pi")) {
             return "3.14" + changePi(str.substring(2));
         }
+
+        return str.charAt(0)+ changePi(str.substring(1));
+
     }
 }
