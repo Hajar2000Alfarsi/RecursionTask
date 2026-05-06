578eedc9bd1f57561343f8fe7f54c6ae21de4b0a
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 22:07:50 +0400

    If for check 8's

--- src/Count8.java
+++ src/Count8.java
@@ -7,5 +7,13 @@ public class Count8 {
         int lastDigit = n % 10;
         int secondLastDigit = (n / 10) % 10;
 
+        if (lastDigit == 8) {
+            if (secondLastDigit == 8) {
+                return 2 + count8(n / 10);
+            } else {
+                return 1 + count8(n / 10);
+            }
+        }
+        return count8(n / 10);
     }
 }
