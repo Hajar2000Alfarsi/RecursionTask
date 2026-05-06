c382e45e03105d64eb32927781221dadf5facc31
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 21:39:40 +0400

    Dsiplay answer of Test example

--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -5,5 +5,9 @@ public class TriangleDemo {
         int result1 = triangle.triangle(0);
         int result2 = triangle.triangle(1);
         int result3 = triangle.triangle(2);
+
+        System.out.println("triangle(0) → " + result1);
+        System.out.println("triangle(1) → " + result2);
+        System.out.println("triangle(2) → " + result3);
     }
 }
