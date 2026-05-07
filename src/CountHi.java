170fcc6f80e4c80b310e3d4ffc033d3730c3735e
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:07:04 +0400

    If first two char are 'hi' count 1

--- src/CountHi.java
+++ src/CountHi.java
@@ -3,5 +3,9 @@ public class CountHi {
         if (str.length() < 2) {
             return 0;
         }
+
+        if (str.substring(0,2).equals("hi")) {
+            return 1 + countHi(str.substring(2));
+        }
     }
 }
