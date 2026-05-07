f97392f57c3c0f3521d05b4f6004189718003518
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:35:55 +0400

    Display Result of Test Cases

--- src/ParenBitDemo.java
+++ src/ParenBitDemo.java
@@ -6,5 +6,8 @@ public class ParenBitDemo {
         String result2 = parenBit.parenBit("x(hello)");
         String result3 = parenBit.parenBit("(xy)1");
 
+        System.out.println("parenBit(\"xyz(abc)123\") → " + result1);
+        System.out.println("parenBit(\"x(hello)\") → " + result2);
+        System.out.println("parenBit(\"(xy)1\") → " + result3);
     }
 }
