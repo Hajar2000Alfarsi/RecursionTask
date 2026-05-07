0c9c9d691fef224dbdc10c860a48d9b87f9d8beb
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 09:03:57 +0400

    Test cases

--- src/FibonacciDemo.java
+++ src/FibonacciDemo.java
@@ -1,5 +1,11 @@
 public class FibonacciDemo {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
+
+        int result1 = fibonacci.fibonacci(0);
+        int result2 = fibonacci.fibonacci(1);
+        int result3 = fibonacci.fibonacci(2);
+        int result4 = fibonacci.fibonacci(5);
+        int result5 = fibonacci.fibonacci(10);
     }
 }
