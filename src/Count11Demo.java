824933ea7c4d736991ef275e085140b4a2764825
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:23:52 +0400

    Test case

--- src/Count11Demo.java
+++ src/Count11Demo.java
@@ -1,5 +1,11 @@
 public class Count11Demo {
     public static void main(String[] args) {
         Count11 count11 = new Count11();
+
+        int result1 = count11.count11("11abc11");
+        int result2 = count11.count11("abc11x11x11");
+        int result3 = count11.count11("111");
+
+
     }
 }
