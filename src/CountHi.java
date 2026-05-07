d6ad8b42eb337a049f445b4cbd937666d7af71cd
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:07:40 +0400

    Otherwise move forward by 1

--- src/CountHi.java
+++ src/CountHi.java
@@ -7,5 +7,7 @@ public class CountHi {
         if (str.substring(0,2).equals("hi")) {
             return 1 + countHi(str.substring(2));
         }
+
+        return countHi(str.substring(1));
     }
 }
