fef88f6f2668ebd3e28a0ffe85e3ecb0e930fc0c
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:09:59 +0400

    Test Cases

--- src/CountHiDemo.java
+++ src/CountHiDemo.java
@@ -1,5 +1,12 @@
 public class CountHiDemo {
     public static void main(String[] args) {
         CountHi countHi = new CountHi();
+
+        int result1 = countHi.countHi("xxhixx");
+        int result2 = countHi.countHi("xhixhix");
+        int result3 = countHi.countHi("hi");
+
+
+
     }
 }
