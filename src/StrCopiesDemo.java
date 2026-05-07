d6375dc010aeeeef697afec7b2968d864bbbecfb
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:55:42 +0400

    Display result of Test Cases

--- src/StrCopiesDemo.java
+++ src/StrCopiesDemo.java
@@ -5,5 +5,10 @@ public class StrCopiesDemo {
         boolean result1 = strCopies.strCopies("catcowcat", "cat", 2);
         boolean result2 = strCopies.strCopies("catcowcat", "cow", 2);
         boolean result3 = strCopies.strCopies("catcowcat", "cow", 1);
+
+        System.out.println("strCopies(\"catcowcat\", \"cat\", 2) → " + result1);
+        System.out.println("strCopies(\"catcowcat\", \"cow\", 2) → " + result2);
+        System.out.println("strCopies(\"catcowcat\", \"cow\", 1) → " + result3);
+
     }
 }
