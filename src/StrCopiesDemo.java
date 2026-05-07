aa7b1836aba4a57d65690ba2470b92fd18aa3bfe
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:55:05 +0400

    Test Cases

--- src/StrCopiesDemo.java
+++ src/StrCopiesDemo.java
@@ -1,5 +1,9 @@
 public class StrCopiesDemo {
     public static void main(String[] args) {
-`       StrCount strCount = new StrCount();
+`       StrCopies strCopies = new StrCopies();
+
+        boolean result1 = strCopies.strCopies("catcowcat", "cat", 2);
+        boolean result2 = strCopies.strCopies("catcowcat", "cow", 2);
+        boolean result3 = strCopies.strCopies("catcowcat", "cow", 1);
     }
 }
