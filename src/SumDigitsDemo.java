94e0842f49d332ae6d93ce330277d055f21ee6ce
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 09:35:39 +0400

    Display result of Test cases

--- src/SumDigitsDemo.java
+++ src/SumDigitsDemo.java
@@ -5,6 +5,10 @@ public class SumDigitsDemo {
         int result1 = sumDigits.sumDigits(126);
         int result2 = sumDigits.sumDigits(49);
         int result3 = sumDigits.sumDigits(12);
+
+        System.out.println("sumDigits(126) → " + result1);
+        System.out.println("sumDigits(49) → " + result2);
+        System.out.println("sumDigits(12) → " + result3);
     }
 
 }
