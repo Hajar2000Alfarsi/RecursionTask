684f9fe53563aa92af0227f3f0d90147b16624d5
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:35:32 +0400

    Test Cases

--- src/ParenBitDemo.java
+++ src/ParenBitDemo.java
@@ -2,5 +2,9 @@ public class ParenBitDemo {
     public static void main(String[] args) {
         ParenBit parenBit = new ParenBit();
 
+        String result1 = parenBit.parenBit("xyz(abc)123");
+        String result2 = parenBit.parenBit("x(hello)");
+        String result3 = parenBit.parenBit("(xy)1");
+
     }
 }
