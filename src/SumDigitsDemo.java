948074ac52c7dd0ccc1ec569f98f40573bcf8ca9
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 09:35:08 +0400

    Test cases

--- src/SumDigitsDemo.java
+++ src/SumDigitsDemo.java
@@ -2,6 +2,9 @@ public class SumDigitsDemo {
     public static void main(String[] args) {
         SumDigits sumDigits = new SumDigits();
 
+        int result1 = sumDigits.sumDigits(126);
+        int result2 = sumDigits.sumDigits(49);
+        int result3 = sumDigits.sumDigits(12);
     }
 
 }
