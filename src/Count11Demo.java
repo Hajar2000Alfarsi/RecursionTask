f8244f14569f5bf6f8f522614837e876a5165a13
Hajar <hajar2000alfarsi@gmail.com>
2026-05-07 08:24:14 +0400

    Display result ofTest case

--- src/Count11Demo.java
+++ src/Count11Demo.java
@@ -6,6 +6,8 @@ public class Count11Demo {
         int result2 = count11.count11("abc11x11x11");
         int result3 = count11.count11("111");
 
-
+        System.out.println("count11(\"11abc11\") → " + result1);
+        System.out.println("count11(\"abc11x11x11\") → " + result2);
+        System.out.println("count11(\"111\") → " + result3);
     }
 }
